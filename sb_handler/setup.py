28368f90cecdf16ab6e054b1e43b08b3788cec5d
petermckeever <43302432+petermckeever@users.noreply.github.com>
2018-09-16 00:09:24 +0200

    Update setup.py

--- sb_handler/setup.py
+++ sb_handler/setup.py
@@ -5,7 +5,6 @@ setup(name='sb_handler',
       description='A package to work with SB Data',
       url='coming_soon',
       author='Peter McKeever',
-      author_email='petermckeever@live.com',
       license='MIT',
       packages=['sb_handler'],
       zip_safe=False)
