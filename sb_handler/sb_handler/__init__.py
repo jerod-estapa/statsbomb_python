84332e0208a1b851f375d17439ba3cd83e01eea3
petermckeever <43302432+petermckeever@users.noreply.github.com>
2018-09-15 23:31:27 +0200

    Add files via upload

--- sb_handler/sb_handler/__init__.py
+++ sb_handler/sb_handler/__init__.py
@@ -31,6 +31,7 @@ def clean_sb(data):
     rec = []
     tm = []
     ptm = []
+    h = []
 
     pat = []
 
@@ -136,6 +137,13 @@ def clean_sb(data):
             ptm.append(data[i]['possession_team']['name'])
         else:
             ptm.append(None)
+        if ("pass" in data[i]):
+            if('height' in data[i]['pass']):
+                h.append(data[i]['pass']['height']['name'])
+            else:
+                h.append(None)
+        else:
+            h.append(None)
 
 
 
@@ -154,6 +162,7 @@ def clean_sb(data):
     match_events['y'] = y
     match_events['end_x'] = end_x 
     match_events['end_y'] = end_y
+    match_events['height'] = h
     #match_events['outcome'] = out
 
     #i = 0
@@ -599,8 +608,193 @@ def shot_map(data, team):
     
 
     plt.savefig("shot_map_"+str(team)+"_vs_"+str(opposition),bbox_inches='tight',dpi=300)
-    plt.show()
     print("this viz is saved as 'shot_map_"+str(team)+"_vs_"+str(opposition)+".png'")
+    plt.show()
+    
+
+def player_pass(df,player_name):
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns; sns.set()
+    from tqdm import tqdm
+    import matplotlib.pyplot as plt
+    
+
+
+    p_pass = df[(df['player'] == player_name)&(df['type'] == 'Pass')]
+
+    p_pass = p_pass[['player','x','y','end_x','end_y','height']]
+
+    p_passg = p_pass[p_pass['height'] == 'Ground Pass']
+    p_passg.reset_index(inplace=True)
+    p_passl = p_pass[p_pass['height'] == 'Low Pass']
+    p_passl.reset_index(inplace=True)
+    p_passh = p_pass[p_pass['height'] == 'High Pass']
+    p_passh.reset_index(inplace=True)
+
+
+    #inc_p_pass = p_pass[p_pass['outcome'] == 'Incomplete']
+
+    #teams = shots.team.unique()
+
+    #if team == teams[0]:
+    #    opposition = teams[1]
+    #else:
+    #    opposition = teams[0]
+
+
+
+
+
+
+    fig,ax2 = plt.subplots(figsize=(9.8,12.4))
+    #ax2 = plt.axes([0, 0, 1.5, 0.5])
+    #fig.figsize(6.8,10.4)
+    ax2.axis('off')
+
+
+
+
+                ### TOP ###
+    vcx1 = [0,0,68,68,0]            # sidelines
+    vcy1 = [0,104,104,0,0]
+
+    vcx2 = [13.84,13.84,54.16,54.16] # outer box
+    vcy2 = [104,87.5,87.5,104]
+
+    vcx3 = [30.34,30.34,37.66,37.66] # goal
+    vcy3 = [104,104.2,104.2,104]
+
+    vcx4 = [24.84,24.84,43.16,43.16] # 6-y box
+    vcy4 = [104,99.5,99.5,104]
+
+    vcx5 = [0,68] # Half-way-line
+    vcy5 = [52,52]
+
+
+                ### BOTTOM ###
+
+    vcx6 = [13.84,13.84,54.16,54.16] # outer box
+    vcy6 = [0,16.5,16.5,0]
+
+    vcx7 = [30.34,30.34,37.66,37.66] # goal
+    vcy7 = [0,-0.2,-0.2,0]
+
+    vcx8 = [24.84,24.84,43.16,43.16] # 6-y box
+    vcy8 = [0,4.5,4.5,0]
+
+
+    #CENTRE CIRCLE
+
+    circle3 = plt.Circle((34, 52), 9.15,ls='solid',lw=1.5,color='black', fill=False, zorder=1,alpha=1)
+
+
+    ###### BOX #######
+    plt.plot(vcx1,vcy1,c='black',zorder=5)
+    plt.plot(vcx2,vcy2,c='black',zorder=5)
+    plt.plot(vcx3,vcy3,c='black',zorder=5)
+    plt.plot(vcx4,vcy4,c='black',zorder=5)
+    plt.plot(vcx5,vcy5,c='black',zorder=5)
+    plt.plot(vcx6,vcy6,c='black',zorder=5)
+    plt.plot(vcx7,vcy7,c='black',zorder=5)
+    plt.plot(vcx8,vcy8,c='black',zorder=5)
+
+    plt.scatter(34,93,c='black',zorder=5)
+    plt.scatter(34,11,c='black',zorder=5)
+    plt.scatter(34,52,c='black',zorder=5)
+
+    circle1 = plt.Circle((34, 93.5), 9.15,ls='solid',lw=1.5,color='black', fill=False, zorder=1,alpha=1)
+    circle2 = plt.Circle((34, 10.5), 9.15,ls='solid',lw=1.5,color='black', fill=False, zorder=1,alpha=1)
+    ax2.add_artist(circle1)
+    ax2.add_artist(circle2)
+    ax2.add_artist(circle3)
+    rec1 = plt.Rectangle((20, 87.5), 30,16,ls='-',color='white', zorder=1,alpha=1)
+    rec2 = plt.Rectangle((0,0),68,104,color='grey',zorder=2,alpha=0.2)  
+    rec3 = plt.Rectangle((20, 0), 30,16.5,ls='-',color='white', zorder=1,alpha=1)
+
+    ax2.add_artist(rec1)
+    ax2.add_artist(rec2)
+    ax2.add_artist(rec3)
+
+
+    xgp = p_passg['x']/120 * 104
+    ygp = p_passg['y']/80 * 68
+    end_xgp = p_passg['end_x']/120 * 104
+    end_ygp = p_passg['end_y']/80 * 68
+
+    xlp = p_passl['x']/120 * 104
+    ylp = p_passl['y']/80 * 68
+    end_xlp = p_passl['end_x']/120 * 104
+    end_ylp = p_passl['end_y']/80 * 68
+
+    xhp = p_passh['x']/120 * 104
+    yhp = p_passh['y']/80 * 68
+    end_xhp = p_passh['end_x']/120 * 104
+    end_yhp = p_passh['end_y']/80 * 68
+
+
+
+    x = p_pass.x/120 * 104
+    y = p_pass.y/80 * 68
+
+    y = 68 - y
+
+    ygp = 68 - ygp
+    end_ygp = 68 - end_ygp
+
+    ylp = 68 - ylp
+    end_ylp = 68 - end_ylp
+
+    yhp = 68 - yhp
+    end_yhp = 68 - end_yhp
+
+
+
+    #x_size = np.array(nshots.sb_xg.values)
+
+    plt.ylim(-1,105)
+    plt.xlim(-1,69)
+
+    i = 0
+
+    for i in range(0,len(p_passg)):
+        plt.plot([ygp[i],end_ygp[i]],
+                [xgp[i],end_xgp[i]],c='red',alpha=0.7,zorder=500)
+    i = 0
+    for i in range(0,len(p_passl)):
+        plt.plot([ylp[i],end_ylp[i]],
+                [xlp[i],end_xlp[i]],c='blue',alpha=0.7,zorder=500)
+    i = 0
+    for i in range(0,len(p_passh)):
+        plt.plot([yhp[i],end_yhp[i]],
+                [xhp[i],end_xhp[i]],c='green',alpha=0.7,zorder=500)
+        
+
+    plt.scatter(y,x,color='white',edgecolors='black',linewidths=1.25,zorder=501,s=110)
+    plt.title(str(p_pass['player'].iloc[0])+" Passes", fontsize=18)
+
+
+    ## PLOT LINES FOR LEGEND ##
+
+
+    plt.plot([-100,-100],
+            [-100,-100],c='red',alpha=0.7,zorder=500,label='Ground Pass')
+    plt.plot([-100,-100],
+            [-100,-100],c='blue',alpha=0.7,zorder=500,label='Low Pass')
+    plt.plot([-100,-100],
+            [-100,-100],c='green',alpha=0.7,zorder=500,label='High Pass')
+
+
+
+    img = plt.imread("statsbomb-logo.jpg")
+    ax2.imshow(img, extent=[50,67,1.0275,5.275],zorder=10000)
+    plt.legend(fontsize=12)
+    plt.savefig(str(p_pass['player'].iloc[0])+"_passes",bbox_inches='tight',dpi=300)
+    print("viz saved as "+str(p_pass['player'].iloc[0])+"_passes")
+
+    plt.show()
 
     
+
+
 
